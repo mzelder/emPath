6dfaf9840336769b4d76ff02effba858cd0b53eb
mzelder <maciej.zelder@wp.pl>
2024-04-12 08:46:36 +0200

    comit

--- app/DataFormater/OneEmotionTwoPhotos.py
+++ app/DataFormater/OneEmotionTwoPhotos.py
@@ -22,6 +22,8 @@ def get_random_picture_ids_by_emotion(json_file_path):
 
     # Losowe wybranie dwóch Picture ID wraz z Intensity (0-1) z wybranej emocji
     threshold = 0.2
+    
+    selected_picture_data = random.sample(chosen_emotion_picture_data, 2)
     while abs(selected_picture_data[0]['Intensity (0-1)'] - selected_picture_data[1]['Intensity (0-1)']) < threshold:
         selected_picture_data = random.sample(chosen_emotion_picture_data, 2)
 
